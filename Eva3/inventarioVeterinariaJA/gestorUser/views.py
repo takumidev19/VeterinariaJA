d42bd95ac8a4a40135b2949a5124943fa660f7cd
takumidev19 <151186342+takumidev19@users.noreply.github.com>
2024-11-29 17:19:14 -0300

    Configuracion inicial
    Se configuro el archivo settings, ademas de crear las carpetas templates y static con sus respectivos archivos

---
+++ Eva3/inventarioVeterinariaJA/gestorUser/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from rest_framework.reverse import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
