0f9c01fd3d118a5c98cab3722d625606f56fb47e
takumidev19 <151186342+takumidev19@users.noreply.github.com>
2024-11-30 14:30:48 -0300

    Configuracion de datatable
    Configure el datatable para que muestre distintas opciones si eres superuser o no, ademas de que los usuarion normales solo pueden ver los productos que ellos crearon

--- Eva3/inventarioVeterinariaJA/gestorProductos/models.py
+++ Eva3/inventarioVeterinariaJA/gestorProductos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='productos')
+
+    def __str__(self):
+        return self.nombre
--- Eva3/inventarioVeterinariaJA/gestorProductos/views.py
+++ Eva3/inventarioVeterinariaJA/gestorProductos/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from gestorProductos.models import Producto, Categoria
 
 # Create your views here.
 def productos_list(request):
-    return render(request, 'productos.html')
+    if request.user.is_superuser:
+        productos = Producto.objects.all()
+    else:
+        productos = Producto.objects.filter(usuario=request.user)
+    return render(request, 'productos.html', {'productos': productos})
 
 def categorias_list(request):
-    return render(request, 'categorias.html')
+    categorias = Categoria.objects.all()
+    return render(request, 'categorias.html', {'categorias': categorias})
